62b0aef88a7ec49d2ad5a18be6265a90a2d5760d
robertofrattini <41764268+robertofrattini@users.noreply.github.com>
2020-12-24 17:56:07 +0100

    better $string

--- bruzzubot.php
+++ bruzzubot.php
@@ -143,7 +143,7 @@ elseif ($message) {
     $chatId = isset($message['chat']['id'])?$message['chat']['id']:'';
     $chatTitle = isset($message['chat']['title'])?$message['chat']['title']:'';
     $text = isset($message['text'])?$message['text']:'';
-    $string = str_replace(" ","",preg_replace("/[^A-Za-z0-9 ]/",' ', strtolower($text)));
+    $string = " ".str_replace(" ","",preg_replace("/[^A-Za-z0-9 ]/",' ', strtolower($text)))." ";
     $newParticipant = isset($message['new_chat_participant'])?$message['new_chat_participant']:'';
     $replyToMessage = isset($message['reply_to_message'])?$message['reply_to_message']:'';
 
